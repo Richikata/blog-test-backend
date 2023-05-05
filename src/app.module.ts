8ca51cd18d4ec57791d2709ed84b63d60a4c2e17
Richikata <sethpaing.spp@gmail.com>
2023-05-05 12:36:50 +0630

    Update Config, App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogsModule } from './blogs/blogs.module';
+import { AuthorsModule } from './authors/authors.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { TagsModule } from './tags/tags.module';
+import { CategoryModule } from './category/category.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+			isGlobal: true,
+			load: [configuration],
+		}),
+    BlogsModule,
+    AuthorsModule,
+    DatabaseModule,
+    AuthModule,
+    TagsModule,
+    CategoryModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Test example')
+    .setDescription('The Blog Test API description')
+    .setVersion('1.0')
+    .addTag('Blog Test')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+		}),
+  );
+  
+  app.use(json({ limit: '50mb' }));
+	app.use(urlencoded({ extended: true, limit: '50mb' }));
+
   await app.listen(3000);
+  
 }
 bootstrap();
